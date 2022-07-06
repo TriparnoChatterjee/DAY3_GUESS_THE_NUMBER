d91a3f4897107d754b7c112c04e0beefa529cbe4
Triparno Chatterjee <chatterjeetriparno007@gmail.com>
2022-07-06 16:33:12 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var output = document.getElementById("out");
+const button = document.querySelector("#check_btn");
+const guess = Math.floor(Math.random() * 100) + 1;
+// console.log(value);
+// console.log(guess);
+const check = () => {
+  const value = document.querySelector("#in").value;
+  if (value == guess) {
+    output.innerHTML = "Congrats! Got the answer correctly!";
+  } else if (value > guess) {
+    output.innerHTML = "Guess a smaller number";
+  } else {
+    output.innerHTML = "Guess a greater number";
+  }
+};
+button.addEventListener("click", check);
